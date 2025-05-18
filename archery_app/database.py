89c2fc782484a55b28236e76ad9c4eb7518c512c
Knguyn0132 <143194005+Knguyn0132@users.noreply.github.com>
2025-05-18 21:55:19 +1000

    hide database credentials

--- archery_app/database.py
+++ archery_app/database.py
@@ -1,13 +1,18 @@
+import os
 import streamlit as st
 import mysql.connector
 import pandas as pd
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host="feenix-mariadb.swin.edu.au",
-        user="s104844794",
-        password="260804",
-        database="s104844794_db",
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
     )
 
 def initialize_connection():
